b5d62f8a5ec5a882d51e41ed3a9eb0b0ec4fe703
ishuah91 <ishuah91@gmail.com>
2016-06-15 07:20:12 +0300

    Added handler_test.go

---
+++ handlers/handlers_test.go
@@ -0,0 +1,16 @@
+package handlers
+
+import (
+	"net/http/httptest"
+	"net/http"
+	"testing"
+)
+
+func TestIndex(t *testing.T){
+	request, _ := http.NewRequest("GET", "/", nil)
+	response := httptest.NewRecorder()
+	Index(response, request)
+	if response.Code != http.StatusOK {
+        t.Fatalf("Non-expected status code%v:\n\tbody: %v", "200", response.Code)
+    }
+}
