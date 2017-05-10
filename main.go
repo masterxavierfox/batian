5d5cd0d2efd1e1faacc082e04a4c35ab7ce77168
ishuah91 <ishuah91@gmail.com>
2017-05-10 19:10:15 +0300

    humble beginnings

--- main.go
+++ main.go
@@ -1,16 +1,95 @@
 package main
 
 import (
-	"github.com/ishuah/batian/models"
-	"github.com/ishuah/batian/routes"
-	"log"
-	"net/http"
+	"bufio"
+	"os"
+	"github.com/nsf/termbox-go"
 )
 
-func main() {
-	db, _ := models.NewDbManager("batian.db")
-	allroutes := routes.BuildRoutes(db)
-	router := routes.NewRouter(allroutes)
+type screen struct {
+	buffer 	[][]rune
+	pos			int
 
-	log.Fatal(http.ListenAndServe(":5000", router))
+}
+
+
+func (s *screen) height() int {
+	_, height := termbox.Size()
+	if len(s.buffer) < height {
+		return len(s.buffer)
+	}
+	return height
+}
+
+func (s *screen) width() int {
+	width, _ := termbox.Size()
+	return width
+}
+
+func (s *screen) draw() {
+	lines := s.buffer[s.pos:(s.height()+s.pos)]
+	y := 0
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	
+	for _, runes := range lines {
+		for x := 0; x < len(runes); x += 1 {
+			termbox.SetCell(x, y, runes[x], termbox.ColorDefault, termbox.ColorDefault)
+		}
+		termbox.Flush()
+		y += 1
+	}
+}
+
+func (s *screen) load() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		s.buffer = append(s.buffer, []rune(scanner.Text()))
+	}
+}
+
+func (s *screen) moveUp() {
+	if s.pos > 0 {
+		s.pos -= 1
+		s.draw()
+	} else {
+		return
+	}
+}
+
+func (s *screen) moveDown() {
+	if len(s.buffer) > s.height() && s.pos < (len(s.buffer) - s.height()) {
+		s.pos += 1
+		s.draw()
+	}
+}
+
+func main() {
+	err := termbox.Init()
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer termbox.Close()
+
+	s := screen{ pos: 0 }
+
+	s.load()
+	s.draw()
+
+	for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			switch ev.Key {
+			case termbox.KeyEsc:
+				return
+			case termbox.KeyArrowUp:
+				s.moveUp()
+			case termbox.KeyArrowDown:
+				s.moveDown()
+			default:
+				s.draw()
+			}
+		}
+	}
 }
